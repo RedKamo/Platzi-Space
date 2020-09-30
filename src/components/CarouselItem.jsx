f5051d003cd8b1ea7819b93c6750d0909399468b
CamiloASuarez <camiloandressuarez1@gmail.com>
2020-09-30 13:39:09 -0500

    add useState and fetch with api

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,21 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 import '../assets/styles/components/CarouselItem.scss';
-import saturnoV1 from '../assets/static/saturnoV1.jpg';
 import playicon from '../assets/static/playicon.png';
 import pauseicon from '../assets/static/pauseicon.png';
 
-const CarouselItem =()=> (
+const CarouselItem =({cover, title, year, contentRating, duration})=> (
   <div className="carousel-item">
-    <img className="carousel-item__img" src={saturnoV1} alt="card1"/>
+    <img className="carousel-item__img" src={cover} alt={title}/>
   <div className="carousel-item__details">
   <div>
     <img className="carousel-item__icons" src={playicon} alt="" />
     <img className="carousel-item__icons" src={pauseicon} alt="" />
   </div>
-    <p className="carousel-item__details--title">Saturno V</p>
-    <p className="carousel-item__details--subtitle">1967 - 1973 (118t OTB)</p>
+    <p className="carousel-item__details--title">{title}</p>
+    <p className="carousel-item__details--subtitle">{`${year} ${contentRating} ${duration}`}</p>
   </div>
 </div>
 )
+
+CarouselItem.prototype = {
+  cover: PropTypes.string,
+  title: PropTypes.string,
+  year: PropTypes.number,
+  contentRating: PropTypes.string,
+  duration: PropTypes.number,
+}
+
 
 export default CarouselItem;
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,44 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
+import useInitialState from '../hooks/useInitialState'
 
 import '../assets/styles/App.scss';
 
-const App = ()=>(
-  <div className="App">
-    <Header/>
-    <Search/>
+const API = 'http://localhost:3000/initalState'
 
-    <Categories title="My List">
-      <Carousel>
-        <CarouselItem/>
-        <CarouselItem/>
-        <CarouselItem/>
-        <CarouselItem/>
-      </Carousel>
-    </Categories>
+const App = ()=>{
+  const initialState = useInitialState(API)
 
-    <Categories title="Trending">
-      <Carousel>
-        <CarouselItem/>
-        <CarouselItem/>
-        <CarouselItem/>
-      </Carousel>
-    </Categories>
+  return initialState.length === 0 ? <h1>Loading ...</h1> : (
+    <div className="App">
+      <Header/>
+      <Search/>
+      {initialState.mylist.length > 0 &&(
+        <Categories title="My List">
+        <Carousel>
+          {initialState.mylist.map(item =>
+            <CarouselItem key={item.id}{...item} />
+            )}
+        </Carousel>
+      </Categories>
+      )}
 
-    <Categories title=" Platzi Originals">
-      <Carousel>
-        <CarouselItem/>
-        <CarouselItem/>
-      </Carousel>
-    </Categories>
+      <Categories title="Trending">
+        <Carousel>
+          {initialState.trends.map(item => 
+            <CarouselItem key={item.id}{...item} />
+          )}
+        </Carousel>
+      </Categories>
 
-    <Footer/>
-  </div>
-)
+      <Categories title=" Platzi Originals">
+        <Carousel>
+          {initialState.originals.map(item => 
+            <CarouselItem key={item.id}{...item} />
+          )}
+        </Carousel>
+      </Categories>
+      <Footer/>
+    </div>
+  )
+}
 
 export default App;
